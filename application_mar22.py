2b993c0072e83745b5131b066d8c19a9bd16d961
Ilana <ilana.e.strauss@gmail.comgit config --global user.email ilana.e.strauss@gmail.com>
2020-04-29 17:55:54 -0500

    hopefully fixing already existing article problem

--- application_mar22.py
+++ application_mar22.py
@@ -339,12 +339,15 @@ def bootstrap():
                posted = 0
                #return render_template('/error.html', article_url=article.url)
 
-            article_list = Article.query.filter_by(image_url=article.image_url)
+            # article_list = Article.query.filter_by(image_url=article.image_url)
+            article_list = Article.query.filter_by(url=article.url)
+
             if posted == 1:
                 flash('Record was successfully added')
             else:
                 db.session.rollback()
-                article_list = Article.query.filter_by(image_url=article.image_url)
+                # article_list = Article.query.filter_by(image_url=article.image_url)
+                article_list = Article.query.filter_by(url=article.url)
                 article=article_list[0]
 
             print ("article.id=" + str(article.id))
