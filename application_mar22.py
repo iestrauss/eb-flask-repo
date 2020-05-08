51db643e41f09fb73ff267b33e3520ee200384e9
Ilana <ilana.e.strauss@gmail.comgit config --global user.email ilana.e.strauss@gmail.com>
2020-05-07 19:52:02 -0500

    part 2 of adding to requireonments

--- application_mar22.py
+++ application_mar22.py
@@ -342,7 +342,7 @@ def bootstrap():
             news_link = url_parse.unquote(url).split("?u=")[1].split("?fbclid")[0]
             print("here comes the news_link")
             print(news_link)
-            import urllib.requests as requests
+            import requests
             final_link = requests.get(news_link)
             print("here comes the final_link.url")
             print(final_link.url)
